5bb48fbf662a4edb35c8e15ecafc3d980a92bd7f
Daniel WB Mazzone <dwbm@danielwbmazzone.com>
2011-06-30 21:37:01 -0400

    Finished layout and routes

---
+++ example_generations.rb
@@ -0,0 +1,6 @@
+person1 = {:first => "FirstPerson", :last => "One"}
+person2 = {:first => "SecondPerson", :last => "Two"}
+person3 = {:first => "ThirdPerson", :last => "Three"}
+
+params = {}
+params = :father => person1
